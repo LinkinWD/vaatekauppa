751665a86aea633399a5afb7f487af04bd683447
Jarmo Lindström <yaramodesu@gmail.com>
2022-11-02 14:21:30 +0200

    categories ja product liitetty alustavasti

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {  useEffect, useState } from 'react'
 
 import styles from '../styles/Shop.module.css'
 
@@ -6,7 +6,30 @@ import styles from '../styles/Shop.module.css'
 import Model from '../public//img/model.png'
 
 const Kauppa = () => {
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+
+  //json server käytässä json-server --watch data.json --port 6000
+  useEffect(()=> {
+    fetch('http://localhost:8000/products')
+    .then(res=> res.json())
+    
+    .then(data => setProducts(data) )
+  },[])
+
+  useEffect(()=> {
+      products.map((product) => {
+        const {category} = product
+        if(categories.indexOf(category) === -1){
+          setCategories(oldarray => [...oldarray, category])
+        }
+      })
+  },[products])
+  
+  
+
   return (
+    <>
     <section className={styles.section}>
       <div className={styles.text_side}>
         <h2>Vaatetori</h2>
@@ -16,6 +39,18 @@ const Kauppa = () => {
         <img src={Model} alt="Mallin kuva" />
       </div>
     </section>
+    
+    <div>
+    {categories.map((cat, idx)=> {
+      return <p>{cat}</p>     
+})}    
+<div>{products.map((product) => {
+    return <p>{product.name}</p>
+})}
+    </div>
+   
+</div>
+    </>
   )
 }
 
